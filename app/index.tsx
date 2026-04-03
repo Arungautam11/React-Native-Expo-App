c65ef1bc679b46b71fe0812c883c5d6e3eebef4d
ArunGautam <asgautam19@gmail.com>
2026-04-03 11:59:51 +0530

    init commit with component done, text,input,switch,pressable

---
+++ app/index.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Button, Pressable, Switch, Text, View } from "react-native";
+
+function HomeScreen() {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+
+  const pressMe = () => {
+    alert("Button Pressed..!!");
+  };
+
+  const onPressFunction = () => {
+      alert("Text Pressed..!!");
+    };
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Hello..! Home Screen</Text>
+      <Button title="Click Me" onPress={pressMe} />
+
+      <Switch
+        trackColor={{ false: "#767577", true: "#c3f0d9" }}
+        thumbColor={isEnabled ? "#25d366" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        value={isEnabled}
+        onValueChange={toggleSwitch}
+      />
+
+      <Pressable onPress={onPressFunction}>
+        <Text>I'm pressable!</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default HomeScreen;

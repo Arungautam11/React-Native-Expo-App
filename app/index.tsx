af1e0434ca42bd6e2270d48dcc71a7760ca05e58
ArunGautam <asgautam19@gmail.com>
2026-04-03 17:03:52 +0530

    initial commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,104 +1,24 @@
-import { useState } from "react";
-import {
-  Button,
-  FlatList,
-  Image,
-  Pressable,
-  StatusBar,
-  StyleSheet,
-  Switch,
-  Text,
-  View,
-} from "react-native";
+import Colors from "@/constants/Colors";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-function HomeScreen() {
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
-
-  const pressMe = () => {
-    alert("Button Pressed..!!");
-  };
-
-  const onPressFunction = () => {
-    alert("Text Pressed..!!");
-  };
-
-  type ItemProps = {
-    title: string;
-  };
-
-  const Item = ({ title }: ItemProps) => (
-    <View style={styles.item}>
-      <Text style={styles.title}>{title}</Text>
-    </View>
-  );
-
-  const DATA = [
-    {
-      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-      title: "First Item",
-    },
-    {
-      id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-      title: "Second Item",
-    },
-    {
-      id: "58694a0f-3da1-471f-bd96-145571e29d72",
-      title: "Third Item",
-    },
-    {
-      id: "58694a0f-3da1-471f-bdsk6-145571e29d72",
-      title: "Fourth Item",
-    },
-  ];
+const Index = () => {
+  const insets = useSafeAreaInsets();
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Hello..! Home Screen</Text>
-      <Button title="Click Me" onPress={pressMe} />
-
-      <Switch
-        trackColor={{ false: "#767577", true: "#c3f0d9" }}
-        thumbColor={isEnabled ? "#25d366" : "#f4f3f4"}
-        ios_backgroundColor="#3e3e3e"
-        value={isEnabled}
-        onValueChange={toggleSwitch}
-      />
-
-      <Pressable onPress={onPressFunction}>
-        <Text>I'm pressable!</Text>
-      </Pressable>
-
-      <Image
-        style={{
-          height: 150,
-          width: 150,
-        }}
-        source={require("../assets/images/google.png")}
-      />
-
-      <FlatList
-        data={DATA}
-        renderItem={({ item }) => <Item title={item.title} />}
-        keyExtractor={(item) => item.id}
-      />
+    <View style={[styles.container, { paddingTop: insets.top }]}>
+      <StatusBar style="light" />
+      <Text>Index</Text>
     </View>
   );
-}
+};
+
+export default Index;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  item: {
-    backgroundColor: "#d7e427",
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 25,
+    backgroundColor: Colors.background,
   },
 });
-
-export default HomeScreen;
